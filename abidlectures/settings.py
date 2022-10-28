fa6c306f0bc81ac9cf76238ef783605907c4fa50
ShafinOhin <shafinohin04@gmail.com>
2022-10-29 03:57:07 +0600

    COrrected it againnnnn

--- abidlectures/settings.py
+++ abidlectures/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECURITY_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default = True, cast = bool)
 
-ALLOWED_HOSTS = ['128.199.149.88', '127.0.0.1', 'www.abidstutorials.com', 'abidslectures.com']
+ALLOWED_HOSTS = ['128.199.149.88', '127.0.0.1', 'www.abidstutorials.com', 'abidstutorials.com']
 
 
 # Application definition
