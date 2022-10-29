fe3daefe0ff2e72246fa0132d8b0c352e8e83bf1
ShafinOhin <shafinohin04@gmail.com>
2022-10-30 00:48:36 +0600

    ball

--- abidlectures/settings.py
+++ abidlectures/settings.py
@@ -195,6 +195,6 @@ else:
     AWS_DEFAULT_ACL = 'public-read'
 
 
-    STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+    STATIC_URL = '{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
