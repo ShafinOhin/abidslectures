1f577dcb20ee49d0a3c28a99b0e5024cbdd5d1ba
ShafinOhin <shafinohin04@gmail.com>
2022-10-30 04:51:13 +0600

    ajaira commmit

--- custom_storages.py
+++ custom_storages.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 from storages.utils import setting
 from django.utils.deconstruct import deconstructible
+from django import template
 
 @deconstructible
 class PrivStorage(S3Boto3Storage):
@@ -13,3 +14,18 @@ class PrivStorage(S3Boto3Storage):
     location = settings.STATICFILES_LOCATION
 
 s3_priv_storage = PrivStorage()
+
+
+from django.conf import settings
+
+register = template.Library()
+@register.filter
+def cdn_url(value):
+    if settings.DEVELOPMENT:
+        return value
+    if settings.AWS_S3_ENDPOINT_URL in value:
+        cdn_domain = 'https://' + settings.AWS_S3_CUSTOM_DOMAIN
+        new_url = value.replace(settings.AWS_S3_ENDPOINT_URL, cdn_domain)
+        return new_url
+    else:
+        return value
